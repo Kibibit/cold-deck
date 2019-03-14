18448c5237b0f54c7b2e7296e384015937aaa670
Neil Kalman <neilkalman@gmail.com>
2019-03-14 03:54:04 +0200

    tests: add tests and coverage

--- src/cold-deck.test.ts
+++ src/cold-deck.test.ts
@@ -1,57 +1,74 @@
-import * as admin from 'firebase-admin';
-
 import { ColdDeck } from './cold-deck';
-import { kbEnv } from './env.service';
 
-kbEnv.config();
+it('should create an instance successfully', () => {
+  const instance = new ColdDeck();
 
-console.log('got the host?', process.env.DB_HOST);
-
-const serviceAccount = require(process.env.FIREBASE_CREDENTIAL_FILE || '');
-
-if (!process.env.PROJECT_ID) { throw new Error('project id must be defined'); }
-
-const firebaseSettings = {
-  // FIREBASE
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  credential: admin.credential.cert(serviceAccount)
-};
-
-const basicLogger = new ColdDeck({
-  path: './logs',
-  firebase: firebaseSettings
+  return expect(instance).toBeTruthy();
 });
 
-const nana = basicLogger.createBasic({ persist: true });
+it('should create a basic logger and log a message', () => {
+  const instance = new ColdDeck();
 
-nana.info('this is from the test file!');
+  const logger = instance.createBasic();
 
-const childLogger = basicLogger.child({ persist: true });
+  return expect(logger).toBeTruthy();
+});
 
-childLogger.info('this is a child!', { service: 'tits', ass: 'is nice' });
+it('should successfully create child logger', () => {
+  const instance = new ColdDeck();
+
+  const logger = instance.child({});
+
+  return expect(logger).toBeTruthy();
+});
+
+// console.log('got the host?', process.env.DB_HOST);
+
+// const serviceAccount = require(process.env.FIREBASE_CREDENTIAL_FILE || '');
+
+// if (!process.env.PROJECT_ID) { throw new Error('project id must be defined'); }
+
+// const firebaseSettings = {
+//   // FIREBASE
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   databaseURL: process.env.DATABASE_URL,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   credential: admin.credential.cert(serviceAccount)
+// };
+
+// const basicLogger = new ColdDeck({
+//   path: './logs',
+//   firebase: firebaseSettings
+// });
+
+// const nana = basicLogger.createBasic({ persist: true });
+
+// nana.info('this is from the test file!');
+
+// const childLogger = basicLogger.child({ persist: true });
+
+// childLogger.info('this is a child!', { service: 'tits', ass: 'is nice' });
 
 
-/* jshint -W098 */
-(function () {
-  const express = require('express'),
-    app = express();
+// /* jshint -W098 */
+// (function () {
+//   const express = require('express'),
+//     app = express();
 
-  app.set('port', 11109);
+//   app.set('port', 11109);
 
-  app.use(basicLogger.expressLogger({ persist: true })); //Log each request
+//   app.use(basicLogger.expressLogger({ persist: true })); //Log each request
 
-  var port = app.get("port");
+//   var port = app.get("port");
 
-  app.get('/', function (req, res) {
-    res.send('hello world');
-  });
+//   app.get('/', function (req, res) {
+//     res.send('hello world');
+//   });
 
-  app.listen(port, function () {
-    nana.info('Server listening at port ' + port);
-  });
-})();
+//   app.listen(port, function () {
+//     nana.info('Server listening at port ' + port);
+//   });
+// })();
--- src/firebase.transport.ts
+++ src/firebase.transport.ts
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase-admin';
 import { omit } from 'lodash';
-
 const Transport = require('winston-transport');
 
-//
-// Inherit from `winston-transport` so you can take advantage
-// of the base functionality and `.exceptions.handle()`.
-//
 export class KbFirebaseTransport extends Transport {
   name: string;
   level: string;
@@ -15,12 +10,7 @@ export class KbFirebaseTransport extends Transport {
 
   constructor(options) {
     super(options);
-    //
-    // Consume any custom options here. e.g.:
-    // - Connection information for databases
-    // - Authentication information for APIs (e.g. loggly, papertrail, 
-    //   logentries, etc.).
-    //
+
     this.name = options.name || 'FirebaseLogger';
     this.level = options.level || 'silly';
     this.ref = options.ref || 'logs';
@@ -30,19 +20,19 @@ export class KbFirebaseTransport extends Transport {
   log(info, callback) {
     const key = info[ this.key ] || Date.now();
 
-    this.ref.update({
-      [ key ]: {
-        level: info.level,
-        message: info.message,
-        meta: omit(info, [ 'level', 'message', this.key ])
-      }
-    }, error => {
-      if (error) {
-        callback(error);
-      } else {
+    this.ref
+      .update({
+        [ key ]: {
+          level: info.level,
+          message: info.message,
+          meta: omit(info, [ 'level', 'message', this.key ])
+        }
+      })
+      .then(() => {
         callback(null, true);
+
         this.emit('logged', info);
-      }
-    });
+      })
+      .catch((error) => callback(error));
   }
 };
