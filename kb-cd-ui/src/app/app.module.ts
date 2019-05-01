a6db9e6112ea419ba861cdeeac4c146985440215
Neil Kalman <neilkalman@gmail.com>
2019-05-01 13:47:02 +0300

    feat: write the unauthenticated route option

--- kb-cd-ui/src/app/app.module.ts
+++ kb-cd-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AnsiPipe } from './ansi.pipe';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule
   ],
